94e582bedd3c20747f19a399187b76d24a649b1b
xiaoxiong-binggan <2010147613@qq.com>
2026-01-02 22:13:35 +0800

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,12 +1,12 @@
 /// <reference types="vite/client" />
 
-// 声明CSS模块类型，解决TS无法识别.module.css文件的报错
+// 声明CSS模块
 declare module '*.module.css' {
   const classes: { [key: string]: string };
   export default classes;
 }
 
-// 可选：声明其他静态资源类型（避免后续导入图片等文件报错）
+// 声明静态资源
 declare module '*.png';
 declare module '*.jpg';
 declare module '*.svg';
