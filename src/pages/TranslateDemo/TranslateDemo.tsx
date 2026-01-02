09b021112213bb6bf3b20186b38190fc7c7a2525
xiaoxiong-binggan <2010147613@qq.com>
2026-01-02 21:40:19 +0800

    Update TranslateDemo.tsx

--- src/pages/TranslateDemo/TranslateDemo.tsx
+++ src/pages/TranslateDemo/TranslateDemo.tsx
@@ -1,3 +1,10 @@
-import React from 'react';
-function TranslateDemo() { return <div>翻译演示页面</div>; }
+// 删除 import React from 'react';
+function TranslateDemo() {
+  return (
+    <div>
+      <h1>翻译演示页面（演示版）</h1>
+    </div>
+  );
+}
+
 export default TranslateDemo;
