132c484ba2f5d96fd2c59fea097711e0c26473c4
xiaoxiong-binggan <2010147613@qq.com>
2026-01-02 20:52:42 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import * as serviceWorkerRegistration from './service-worker-registration.ts';
 
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// 注册Service Worker，实现PWA离线能力
+serviceWorkerRegistration.register();
