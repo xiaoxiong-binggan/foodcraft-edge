edccc6ec292b7a8aeb8c43e46207303b4ec2881d
xiaoxiong-binggan <2010147613@qq.com>
2026-01-02 21:40:56 +0800

    Update UserCenter.tsx

--- src/pages/UserCenter/UserCenter.tsx
+++ src/pages/UserCenter/UserCenter.tsx
@@ -1,3 +1,10 @@
-import React from 'react';
-function UserCenter() { return <div>用户中心页面</div>; }
+// 删除 import React from 'react';
+function UserCenter() {
+  return (
+    <div>
+      <h1>用户中心页面（演示版）</h1>
+    </div>
+  );
+}
+
 export default UserCenter;
