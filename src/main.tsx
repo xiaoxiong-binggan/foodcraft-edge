325452cc80b3236671abc727e16bf2441ddb335d
xiaoxiong-binggan <2010147613@qq.com>
2026-01-03 12:32:50 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-// 直接导入，无需调用register
-import './service-worker-registration.ts';
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
